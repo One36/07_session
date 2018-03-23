ffe3d86c8178339d174953e35e82f672e44cc815
pr11 <pr11@figd-1811711.figdtk.de>
2018-03-23 11:09:14 +0100

    07_session

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+    
+    
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP - 07 Session</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="assets/css/styles.css" rel="stylesheet" type="text/css"/>
+        <script src="assets/js/jquery-3.3.1.min.js" type="text/javascript"></script>
+        <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="assets/js/main.js" type="text/javascript"></script>
+    </head>
+    <body>
+        <h1>PHP - Session</h1>
+        <div class="container">
+            
+        </div>
+    </body>
+</html>
